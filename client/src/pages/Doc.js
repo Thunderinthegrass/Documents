2d97f1f892ad6955029b410711e3ceab68a23031
ThunderInTheGrass <propeller8924@gmail.com>
2022-06-14 20:46:05 +0800

    pognali

---
+++ client/src/pages/Doc.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import {Button, Container} from "react-bootstrap";
+import CreateBrand from "../components/modals/CreateBrand";
+import CreateDevice from "../components/modals/CreateDevice";
+import CreateType from "../components/modals/CreateType";
+
+const Doc = () => {
+  const [brandVisible, setBrandVisible] = useState(false)
+  const [typeVisible, setTypeVisible] = useState(false)
+  const [deviceVisible, setDeviceVisible] = useState(false)
+
+  return (
+      <Container className="d-flex flex-column">
+          <Button
+              variant={"outline-dark"}
+              className="mt-4 p-2"
+              onClick={() => setDeviceVisible(true)}
+          >
+              Заполнить форму
+          </Button>
+          <CreateBrand show={brandVisible} onHide={() => setBrandVisible(false)}/>
+          <CreateDevice show={deviceVisible} onHide={() => setDeviceVisible(false)}/>
+          <CreateType show={typeVisible} onHide={() => setTypeVisible(false)}/>
+      </Container>
+  );
+};
+
+export default Doc;
